ac4df3317779ee0d357fbb07004875fda8e309ef
unifralonso <unifralonso@P3LB0912.FEINET.FEI.EDU.BR>
2025-05-19 20:33:05 -0300

    Aprimoramento de código

---
+++ src/spotifei/dao/Conexao.java
@@ -0,0 +1,14 @@
+package spotifei.dao;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author unifralonso
+ */
+public class Conexao {
+    
+}
---
+++ src/spotifei/entidades/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spotifei.entidades;
+
+/**
+ *
+ * @author unifralonso
+ */
+public class Usuario {
+    
+}
